b848171ac7ef0a43be2f8217a75c5a5324cd252c
Pablo Gándara Vicente <tabagonet@gmail.com>
2025-06-30 17:07:57 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.js
+++ next.config.js
@@ -37,6 +37,10 @@ const nextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'www.google.com',
+      },
+      {
+        protocol: 'https',
         hostname: 'quefoto.es',
       },
       {
